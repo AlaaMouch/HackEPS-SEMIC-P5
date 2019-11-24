0b61312645338339660f0fdff863de2efbfc1ec3
Alaa Moucharrafie <alaa.moucharrafie@gmail.com>
2019-11-24 01:38:25 +0100

    main calculates real sum and salami sliced sum

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 import sys
 
 if len(sys.argv) <= 1:
@@ -19,28 +20,44 @@ def round2dec(val):
 #	return val
 	return math.floor(100.0*val)/100.0
 
-sum = 0.0
+def apply_percentage(val, percentage):
+	return val*((np.float64(100.0) - np.float64(percentage))/np.float64(100.0))
+
+sum = np.float64(0.0)
+sum_s = np.float64(0.0)
 for bill_id in BillDict.keys():
 	bill_instance = BillDict[bill_id]
-	partial_sum = 0.0
+	partial = np.float64(0.0)
+	partial_s = np.float64(0.0)
 	for bill_item_instance in bill_instance.bill_items:
 #		product_item = ProductDict[bill_item_instance.product_id]
-		product_final_cost = 0.0
-		product_final_cost += bill_item_instance.units
-#		product_final_cost *= product_item.price_sale
-		product_final_cost *= bill_item_instance.price
-		product_final_cost = round2dec(product_final_cost)
-		product_final_cost *= (100.0 - bill_item_instance.discount_1)/100.0
-		product_final_cost = round2dec(product_final_cost)
-		product_final_cost *= (100.0 - bill_item_instance.discount_2)/100.0
-		product_final_cost = round2dec(product_final_cost)
-		partial_sum += product_final_cost
-	partial_sum *= (100.0 - bill_instance.discount)/100.0
-	partial_sum = round2dec(partial_sum)
-	partial_sum *= (100.0 - bill_instance.soon_payment)/100.0
-	partial_sum = round2dec(partial_sum)
-	partial_sum += bill_instance.shipping_amount
-	partial_sum = round2dec(partial_sum)
-	sum += partial_sum
+		cost = bill_item_instance.units
+#		cost *= product_item.price_sale
+		cost *= bill_item_instance.price
+		cost = apply_percentage(cost, bill_item_instance.discount_1)
+		cost = apply_percentage(cost, bill_item_instance.discount_2)
+		partial += cost
 
-print(sum)
+		cost_s = bill_item_instance.units
+#		cost_s *= product_item.price_sale
+		cost_s *= bill_item_instance.price
+		cost_s = round2dec(apply_percentage(cost_s, bill_item_instance.discount_1))
+		cost_s = round2dec(apply_percentage(cost_s, bill_item_instance.discount_2))
+		partial_s += cost_s
+	partial = apply_percentage(partial, bill_instance.discount)
+	partial = apply_percentage(partial, bill_instance.soon_payment)
+	partial += bill_instance.shipping_amount
+	sum += partial
+
+	partial_s = round2dec(apply_percentage(partial_s, bill_instance.discount))
+	partial_s = round2dec(apply_percentage(partial_s, bill_instance.soon_payment))
+	partial_s += bill_instance.shipping_amount
+	sum_s += partial_s
+
+print("     sum: {}".format(sum))
+print("  sliced: {}".format(sum_s))
+print("    diff: {}".format(abs(sum_s - sum)))
+print()
+
+expected_sum = 1392964.84
+print("expected: {}".format(expected_sum))
