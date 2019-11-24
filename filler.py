262bc1c42de8124aaf57fbddfebaba3006324ef2
Alaa Moucharrafie <alaa.moucharrafie@gmail.com>
2019-11-24 11:26:29 +0100

    delete filler.py

--- filler.py
+++
@@ -1,8 +0,0 @@
-
-from classes import *
-
-BillBillItems = {}
-for bill_item in BillItemDict:
-	if bill_item.bill_id not in BillBillItems:
-		BillBillItems[bill_item.bill_id] = []
-	BillBillItems[bill_item.bill_id].append(bill_item)
